13f915664297946005e1ba2416455eeda6969f24
Olei Eto <teneeto@gmail.com>
2020-01-05 22:47:58 +0100

    add control buttons

--- App.js
+++ App.js
@@ -1,46 +1,69 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, TouchableOpacity } from 'react-native';
 import moment from 'moment';
 
-const DATA = {
-  timer: 1234567,
-  laps: [12345, 2345, 34567, 98765],
-}
-
-function Timer({ interval }) {
+function Timer({ interval, style }) {
+  const pad = (n) => n < 10 ? '0' + n : n
   const duration = moment.duration(interval)
   const centiseconds = Math.floor(duration.milliseconds() / 10)
   return (
-    <Text style={styles.timer}>
-      {duration.minutes()}:{duration.seconds()},{centiseconds}
-    </Text>
+    <View style={styles.timerContainer}>
+      <Text style={style}>{pad(duration.minutes())}:</Text>
+      <Text style={style}>{pad(duration.seconds())},</Text>
+      <Text style={style}>{pad(centiseconds)}</Text>
+    </View>
+
   )
 }
 
-function RoundButton({ title, color, background }) {
+function RoundButton({ title, color, background, onPress, disabled }) {
   return (
-    <View style={[styles.button, { backgroundColor: background }]}>
+    <TouchableOpacity
+      onPress={() => !disabled && onPress()}
+      style={[styles.button, { backgroundColor: background }]}
+      activeOpacity={disabled ? 1.0 : 0.7}>
       <View style={styles.buttonBorder}>
         <Text style={[styles.buttonTitle, { color }]} >{title}</Text>
       </View>
+    </TouchableOpacity>
+  )
+}
+
+function Lap({ number, interval, fastest, slowest }) {
+  const lapStyle = [
+    styles.lapText,
+    fastest && styles.fastest,
+    slowest && styles.slowest,
+  ]
+  return (
+    <View style={styles.lap}>
+      <Text style={lapStyle}> Lap {number}</Text>
+      <Timer style={lapStyle} interval={interval} />
     </View>
   )
 }
 
-function Lap({ number, interval }) {
+function LapsTable({ laps, timer }) {
+  const finishedLaps = laps.slice(1);
+  let max = Number.MIN_SAFE_INTEGER;
+  let min = Number.MAX_SAFE_INTEGER;
+  if (finishedLaps.length >= 2) {
+    finishedLaps.forEach(lap => {
+      if (lap < min) min = lap;
+      if (lap > max) max = lap;
+    })
+  }
   return (
-    <View style={styles.lap}>
-      <Text style={styles.lapText}> Lap {number}</Text>
-      <Text style={styles.lapText}>{interval}</Text>
-    </View>
-  )
-}
-function LapsTable({ laps }) {
-  return (
-    <ScrollView>
-      {laps.map((lap, index) => {
-        <Lap number={index} interval={lap} />
-      })}
+    <ScrollView style={styles.scrollView}>
+      {laps.map((lap, index) => (
+        <Lap
+          number={laps.length - index}
+          key={laps.length - index}
+          interval={index === 0 ? lap + timer : lap}
+          fastest={lap === min}
+          slowest={lap === max}
+        />
+      ))}
     </ScrollView>
   )
 }
@@ -52,14 +75,47 @@ function ButtonsRow({ children }) {
 }
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      start: 0,
+      now: 0,
+      laps: []
+    }
+  }
+
+  start = () => {
+    const now = new Date().getTime();
+    this.setState({
+      start: now,
+      now,
+      laps: [0]
+    })
+    this.timer = setInterval(() => {
+      this.setState({ now: new Date().getTime() })
+    }, 100)
+
+  }
+
   render() {
+    const { now, start, laps } = this.state;
+    const timer = now - start;
     return (
       <View style={styles.container}>
-        <Timer interval={DATA.timer} />
-        <ButtonsRow>
-          <RoundButton title='Reset' color='#FFFFFF' background='#3D3D3D' />
-          <RoundButton title='Start' color='#50D167' background='#1B361F' />
-        </ButtonsRow>
+        <Timer interval={timer} style={styles.timer} />
+        {laps.length === 0 && (
+          <ButtonsRow>
+            <RoundButton title='Reset' color='#FFFFFF' background='#3D3D3D' />
+            <RoundButton onPress={this.start} title='Start' color='#50D167' background='#1B361F' />
+          </ButtonsRow>
+        )}
+        {start > 0 && (
+          <ButtonsRow>
+            <RoundButton title='Lap' color='#FFFFFF' background='#3D3D3D' />
+            <RoundButton onPress={this.start} title='Stop' color='#E33935' background='#3C1715' />
+          </ButtonsRow>
+        )}
+        <LapsTable laps={laps} timer={timer} />
       </View>
     );
   }
@@ -77,6 +133,7 @@ const styles = StyleSheet.create({
     color: '#FFFFFF',
     fontSize: 76,
     fontWeight: '200',
+    width: 110
   },
   button: {
     width: 80,
@@ -100,13 +157,31 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignSelf: 'stretch',
     justifyContent: 'space-between',
-    marginTop: 80
+    marginTop: 80,
+    marginBottom: 30,
   },
   lap: {
-
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    borderColor: '#151515',
+    borderTopWidth: 1,
+    paddingVertical: 10,
   },
   lapText: {
-
+    color: '#FFFFFF',
+    fontSize: 18,
+    width: 30
+  },
+  scrollView: {
+    alignSelf: 'stretch'
+  },
+  fastest: {
+    color: '#4BC05F'
+  },
+  slowest: {
+    color: '#CC3531',
+  },
+  timerContainer: {
+    flexDirection: 'row'
   }
 });
-
